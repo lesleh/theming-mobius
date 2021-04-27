c0782846e3dde324992c1fd04a8a8a3fa6bb64ee
Leslie Hoare <leslie.hoare@simplybusiness.co.uk>
2021-04-27 10:23:30 +0100

    Be more specific about Mobius component type

--- src/contexts/MobiusComponentContext.ts
+++ src/contexts/MobiusComponentContext.ts
@@ -1,7 +1,13 @@
 import { createContext } from "react";
 import * as Core from "@simplybusiness/mobius-core";
 
-export type MobiusComponentContextType = typeof Core | null;
+export interface MobiusComponents {
+  Box: typeof Core.Box;
+  Button: typeof Core.Button;
+  // Etc
+}
+
+export type MobiusComponentContextType = MobiusComponents | null;
 export const MobiusComponentContext = createContext<MobiusComponentContextType>(
   null
 );
